4f83689d0051efdb9d73483dae8617a173e73ec8
MehediHasan4321 <126mehedi@gmail.com>
2023-02-06 21:45:26 +0600

    problem 4 complet

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//problem no-1
+
 function mindGame(number) {
     let result = 0;
     if (number < 0) {
@@ -11,18 +13,46 @@ function mindGame(number) {
     return result;
 }
 
+//Problem no-2
 
-function evenOdd(string){
-    if(typeof string ==="string"){
-        if(string.length % 2 ===0){
+function evenOdd(string) {
+    if (typeof string === "string") {
+        if (string.length % 2 === 0) {
             return "Event"
-        }else{
+        } else {
             return "Odd"
         }
 
-    }else{
+    } else {
         alert("Plz provide a string to getting right output")
     }
 }
 
-console.log(evenOdd("bal"))
+//Problem no-3
+
+function isLGSeven(number) {
+    let differenc = number - 7
+    if (Math.abs(differenc) < 7) {
+        return differenc
+    } else {
+        return differenc *= 2
+    }
+}
+
+
+//Problem-4
+function findingBadData(arr) {
+    let badDate = 0;
+    let goodDate = 0;
+    for (let i = 0; i< arr.length; i++) {
+        if (arr[i] < 0) {
+            badDate += 1;
+        } else if(arr[i]>=0) {
+            goodDate++
+        }
+
+    }
+    return badDate;
+}
+
+console.log(findingBadData(arr))
